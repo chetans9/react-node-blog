1c363892974f799a11640bbcb56b16c25077fc50
chetans9 <chetanshenai9@gmail.com>
2022-07-11 20:58:15 +0530

    search page wip

--- client/src/pages/SearchPageComponent.js
+++ client/src/pages/SearchPageComponent.js
@@ -1,54 +1,76 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import PostCardComponent from '../components/Posts/PostCardComponent';
-
+import queryString from 'query-string';
 
 function SearchPageComponent(props) {
 
     let [posts, setPosts] = useState([]);
+
+
     let [loadingPosts, setLoadingPosts] = useState(true);
 
     let [formData, setformData] = useState(
         {
-            search: ""
-        });
+            str: "",
+            sort_by: ""
+        }
+    );
 
-    let handleSearchChange = (event) => {
-        setformData({ search: event.target.value });
+    // let handleSearchChange = (event) => {
+    //     setformData({ str: event.target.value });
+    // }
+
+    let set = function(name){
+
+        return (event) => {
+
+            setformData({ [name]: event.target.value });
+        } 
+
+
+
     }
+
+    //const searchData = queryString.parse(window.location.search);
 
 
     useEffect(() => {
 
-
         loadPosts();
-
     }, []);
 
     let loadPosts = () => {
 
-        axios.get(`${process.env.REACT_APP_API_BASE_URL}/posts/search`).then((result) => {
+        let qs = queryString.stringify(formData);
+
+
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/posts/search?${qs}`).then((result) => {
+
             setPosts(result.data.data);
             setLoadingPosts(false);
         });
 
 
 
-
-
-
-
     }
 
     let handleSubmit = (event) => {
-
         event.preventDefault();
+
+        // window.history.pushState(queryString.parse(window.location.search));
+        // setformData(queryString.parse(window.location.search));
+
+
+
+        loadPosts();
+
 
 
     }
 
     return <>
-        <h3>Search page component</h3>
+        <h3>Search page </h3>
 
         <hr></hr>
         <div id="searchFormSection">
@@ -57,19 +79,27 @@ function SearchPageComponent(props) {
                     <div className='col-lg-8'>
 
                         <div className='row'>
-                            <div className='offset-lg-6 col-lg-4 col-sm-4'>
-
-                                <input type='text' className='form-control' onChange={handleSearchChange} value={formData.search} />
+                            <div className='offset-lg-4 col-lg-3 col-sm-3'>
+                                <label>Search</label>
+                                <input type='text' className='form-control' onChange={set('str')} value={formData.search} />
                             </div>
 
                             <div className='col-lg-2 col-sm-2'>
-                                <button type='submit' className='form-control btn btn-primary' onChange={handleSearchChange} value={formData.search}>Search</button>
+                                <label>Order By</label>
+
+                                <select className="form-select" aria-label="Default select example" value={formData.sort_by} onChange={set('sort_by')}>
+                                    <option value="">Select</option>
+                                    <option value={"newest"}>Newest</option>
+                                    <option value={"olodest"}>Oldest</option>
+                                </select>
+                            </div>
+
+                            <div className='col-lg-2 col-sm-2'>
+                                <label><span> </span></label>
+                                <button type='submit' className='form-control btn btn-primary' onChange={handleSubmit} value={formData.search}>Search</button>
 
                             </div>
                         </div>
-
-
-
                     </div>
                 </div>
             </form>
