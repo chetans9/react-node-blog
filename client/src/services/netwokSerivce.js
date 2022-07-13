b3f5b4eb59b0ef013329e9ac0cf66a12338189dc
chetans9 <chetanshenai9@gmail.com>
2022-07-13 11:46:24 +0530

    fix axios interceptor for logn

--- client/src/services/netwokSerivce.js
+++ client/src/services/netwokSerivce.js
@@ -1,63 +1,64 @@
 import axios from 'axios';
-import store  from '../store';
+import store from '../store';
 
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.interceptors.request.use(request => {
+  // Edit request config
+  request.headers.Authorization = `Bearer ${localStorage.getItem('jwt')}`;
+  return request;
+}, error => {
 
-if(process.env.REACT_APP_JWT_MODE === 'local'){
-
-    axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('jwt')}`;
   
-  }
-
-  axios.defaults.headers.post['Content-Type'] = 'application/json';
+  return Promise.reject(error);
+});
 
 
 const responseSuccessHandler = response => {
-    
-    return response;
-  };
+
+  return response;
+};
 
 
-  const responseErrorHandler = (error)  => {
+const responseErrorHandler = (error) => {
 
-    
 
-    if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-      if(error.response.status == 401){
 
-  
-        // localStorage.removeItem('jwt');
-        // localStorage.removeItem('authUser');
-        //window.location.href = "/Login";
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    if (error.response.status == 401) {
 
-        store.dispatch({
-          type: "LOGOUT"
-        });
-        
-      }else if(error.response.status == 500){
-       // window.location.href = "/error";
-  
-      }
-      console.error(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.error(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.error("Error", error.message);
+
+      // localStorage.removeItem('jwt');
+      // localStorage.removeItem('authUser');
+      //window.location.href = "/Login";
+
+      store.dispatch({
+        type: "LOGOUT"
+      });
+
+    } else if (error.response.status == 500) {
+      // window.location.href = "/error";
+
     }
-  
-    return Promise.reject(error);
-  
+    console.error(error.response.headers);
+  } else if (error.request) {
+    // The request was made but no response was received
+    console.error(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.error("Error", error.message);
   }
-  
-  axios.interceptors.response.use(
-    response => responseSuccessHandler(response),
-    error => responseErrorHandler(error)
-  );
+
+  return Promise.reject(error);
+
+}
+
+axios.interceptors.response.use(
+  response => responseSuccessHandler(response),
+  error => responseErrorHandler(error)
+);
 
 
 
 
-  
